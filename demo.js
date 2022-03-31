ffb52ede62602f65c6c994f8cd87bab75ddcafe0
Long Cui <long.cui@kongsbergdigital.com>
2022-03-31 15:39:21 +0200

    https://www.npmjs.com/package/flexsearch#index-documents-field-search

--- demo.js
+++ demo.js
@@ -4,4 +4,20 @@ const { Index, Document, Worker } = require("flexsearch");
 const index = new Index();
 index.add(1, "abc de");
 const result = index.search("abc");
-console.log(result);
+console.log(result);    // [1]
+
+const index2 = new Document({
+    document: { 
+        index: "content",
+        store: ["author", "email"] 
+    }
+});
+
+index2.add({
+    id: 0,
+    author: "Jon Doe",
+    email: "john@mail.com",
+    content: "Some content for the index ..."
+});
+const result2 = index2.search("some content", { enrich: true });        //Enrich IDs from the results with the corresponding documents.
+console.log(JSON.stringify(result2));   
